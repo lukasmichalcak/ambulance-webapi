e83352e92f908109f2910619ab8d60d92e2b7fdf
lukasmichalcak <xmichalcak@stuba.sk>
2026-03-24 14:46:13 +0100

    Kostra servera

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/lukasmichalcak/ambulance-webapi/api"
+	"github.com/lukasmichalcak/ambulance-webapi/internal/ambulance_wl"
 )
 
 func main() {
@@ -21,7 +22,14 @@ func main() {
     }
     engine := gin.New()
     engine.Use(gin.Recovery())
+
     // request routings
+    handleFunctions := &ambulance_wl.ApiHandleFunctions{
+        AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
+        AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+    }
+    ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
+
     engine.GET("/openapi", api.HandleOpenApi)
     engine.Run(":" + port)
 }
